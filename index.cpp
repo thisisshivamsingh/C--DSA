aa70496928053fe1b0bfd7e7a8216bbe053c7875
Shivam Kumar Singh <shivamsingh@Shivams-MacBook-Air.local>
2026-01-04 12:25:06 +0530

    C++ introduction.

--- index.cpp
+++ index.cpp
@@ -2,7 +2,69 @@
 using namespace std;
 
 int main(){
-    int x;
-    cin >> x;
-    cout << "Hey " << x;
+// cout << "Namaste Dunia :-)";
+// cout << "\n";
+
+// int a = 123;
+// cout << a << endl;
+
+// char b = 'v'; 
+// cout << b << endl;
+
+// bool bl = true;
+// cout << bl << endl;
+
+// float f = 1.2;
+// cout << f << endl;
+
+// double d = 1.23;
+// cout << d << endl;
+
+// int size = sizeof(f); 
+// cout << "Size of f is :" << size << endl;
+
+// Type casting //
+
+// int a = 'a';
+// cout << a << endl;
+
+// char ch = 98;
+// cout << ch << endl;
+
+// char ch1 = 123456;
+// cout << ch1 << endl;
+
+// unsigned int nw = -112;
+// cout << nw << endl;
+
+// double a = 2.0/5;
+
+// cout << a << endl;
+
+// cout << 2.0/5 <<endl;
+
+// int a = 2;
+// int b = 3;
+
+// bool first = (a==b);
+// cout << first << endl;
+
+// bool second = (a>b);
+// cout << second << endl;
+
+// bool third = (a<b);
+// cout << third << endl;
+
+// bool fourth = (a>=b);
+// cout << fourth << endl;
+
+// bool fifth = (a<=b);
+// cout << fifth << endl;
+
+// bool sixth = (a!=b);
+// cout << sixth << endl;
+
+int a = 20;
+cout << !a << endl;
+
 }
