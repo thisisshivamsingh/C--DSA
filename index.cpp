6904bd66aec3192c1300775c9cc120bb9085daf1
Shivam Kumar Singh <shivamsingh@Shivams-MacBook-Air.local>
2026-01-06 08:21:06 +0530

    Loops

--- index.cpp
+++ index.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main(){
+// int main(){
 // cout << "Namaste Dunia :-)";
 // cout << "\n";
 
@@ -67,4 +67,162 @@ int main(){
 // int a = 20;
 // cout << !a << endl;
 
+//// Conditions and loops //////
+
+// }
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    // int a;
+    // cin>>a;
+
+    // cout<<"value of n is :"<<n<<endl;
+
+    // if a is positive
+    // if (a>0){
+    //     cout<<"A is positive" << endl;
+    // }
+    // else {
+    //     cout << "A is negative" << endl;
+    // }
+
+    // int a,b;
+
+    // cin >> a >> b;
+
+    // a = cin.get();
+
+    // cout<<"Value of a and b is" << a <<" "<<b<< endl;
+
+    // cout << "value of a is :" << a << endl;
+
+    // cout << "Enter the value of a"<<endl;
+    // cin>>a;
+    // cout<<"Enter the value of b" <<endl;
+    // cin>>b;
+
+    // if(a>b){
+    //     cout << " A is greater" <<a << endl;
+    // }
+    // if(b>a){
+    //     cout<<"B is greater" <<b<< endl;
+    // }
+
+    // int a;
+    // cout << "enter the value of a" << endl;
+    // cin>>a;
+
+    // if(a>0){
+    //     cout << "A is positive";
+    // }else{
+    //     if(a<0){
+    //         cout<<"A is negative";
+    //     }else{
+    //         cout<< "A is zero";
+    //     }
+    // }
+
+    //  if(a>0){
+    //     cout << "A is positive" << endl;
+    // } else if(a<0){
+    //         cout<<"A is negative" << endl;
+    // }else {
+    //     cout<<"A is 0" <<endl;
+    // }
+
+    // int a = 9;
+    // if(a==9){
+    //     cout<<"NINEY";
+    // }
+    // if(a>0){
+    //     cout<<"POSITIVE";
+    // }else{
+    //     cout<< "NEGATIVE";
+    // }
+
+    // int a = 2;
+    // int b = a+1;
+
+    // if((a=3)==b){
+    //     cout<<a<<(a=3);
+    // }else{
+    //     cout<<a+1<<"else";
+    // }
+
+    // int a = 24;
+
+    // if(a>20){
+    //     cout<<"Love ";
+    // }else if(a==24){
+    //     cout<<"Lovely";
+    // }else{
+    //     cout<<"Babber";
+    // }
+
+
+    // char b;
+    // cin >> b;
+    // int a = b;
+    // cout << a << "This is input";
+
+    // if(a>= 97 && a<=122){
+    //     cout<<"This is small alphabet";
+    // }else if(a>=65 && a<=90){
+    //     cout<<"This is capital alphabet";
+    // }else if(a>=48 && a<=57){
+    //     cout<<"This is number";
+    // }
+
+    // int n;
+    // cin >> n;
+
+    // int i = 1;
+
+    // while(i<=n){
+    //     cout <<i<<" ";
+    //     i = i + 1;
+    // }
+
+//     int n;
+//     cin >> n;
+
+//     int sum = 0;
+//     int i = 1;
+
+//     while(i<=n){
+// sum = sum + i;
+// i = i + 1;
+
+//     }
+
+//     cout << "Total sum of input "<< sum;
+
+// int n;
+// cin >> n;
+// int sum = 0;
+// int i = 2;
+
+// while(i<=n){
+//     sum = sum + i;
+//     i = i + 2;
+// }
+
+// cout << "Total even sum of input" << sum;
+
+// int n;
+// cin >> n;
+
+// int i = 2;
+
+// while(i<n){
+//     if(n % i == 0){
+// cout << i << " Not prime" << endl;
+//     }else {
+//         cout << i << " Prime" << endl;
+//     }
+
+//     i = i + 1;
+// }
 }
