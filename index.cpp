7d6a77206797b2436f245475bf380d4cde422f91
Shivam Kumar Singh <shivamsingh@Shivams-MacBook-Air.local>
2026-01-07 07:38:52 +0530

    Patterns part 1.

--- index.cpp
+++ index.cpp
@@ -225,4 +225,33 @@ int main() {
 
 //     i = i + 1;
 // }
+
+// int n;
+// cin >> n;
+
+// int i = 1;
+
+// while(i<=n){
+//     int j = 1;
+//     while(j<=n){
+//     cout<<"*";
+//     j = j + 1;
+//     }
+//     cout<<endl;
+//     i = i + 1;
+// }
+
+int n;
+cin >> n;
+
+int i = 1;
+while(i<=n){
+    int j = 1;
+    while(j<=n){
+cout << i;
+j = j + 1;
+    }
+    cout << endl;
+    i = i + 1;
+}
 }
